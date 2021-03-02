dc2c45f74fe9be7984b7d3b32e605d6ed948a228
Jason Smith <jasuniprime@gmail.com>
2021-03-01 19:55:18 -0700

    Shorten Fibonacci Finder description

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -23,7 +23,6 @@ class Calculator {
     Returns the n'th number in the fibonacci sequence
     https://en.wikipedia.org/wiki/Fibonacci_number
     Example below
-    n = x
     0 = 0
     1 = 1
     2 = 1
